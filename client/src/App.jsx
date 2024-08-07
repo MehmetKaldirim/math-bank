4cf1503043771b7eb78a1a38a8234f7509609fec
Mehmet <mathdenizi@gmail.com>
2024-08-07 12:29:13 +0200

    empty components ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./style";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+      {/* Hero */}
+      <div className="`bg-primary  ${styles.flexStart}`">
+        <div className={`${styles.boxWidth}`}> Hero </div>
+      </div>
+      {/* Hero */}
+      <div className="`bg-primary  ${styles.paddingX} ${styles.flexStart}`">
+        <div className={`${styles.boxWidth}`}>
+          stats Business Billing CardDeal Testimonials Clients CTA Footer
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
